b3ce583cbe0f43ad04530c718803cf842821a38e
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-12-31 16:22:21 +1100

    Initial commit

---
+++ test/rgb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Rgb from '../lib/rgb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rgb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Rgb.RgbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
