5c9a48bf1e027bde982765254ef2af476c21621e
mcteamster <28884260+mcteamster@users.noreply.github.com>
2026-03-16 19:31:03 +1100

    fix: chromium only

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -36,17 +36,13 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
 
-  projects: process.env.CI
-    ? [
-        { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-      ]
-    : [
-        { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-        { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-        { name: 'webkit', use: { ...devices['Desktop Safari'] } },
-        { name: 'mobile-chrome', use: { ...devices['Pixel 5'] } },
-        { name: 'mobile-safari', use: { ...devices['iPhone 12'] } },
-      ],
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    // { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    // { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    // { name: 'mobile-chrome', use: { ...devices['Pixel 5'] } },
+    // { name: 'mobile-safari', use: { ...devices['iPhone 12'] } },
+  ],
 
   /* Automatically start the Vite dev server when running tests locally.
    * Requires `client/.env.local` to exist — see client/.env.local.example.
