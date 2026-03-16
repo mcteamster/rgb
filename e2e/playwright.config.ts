c9685a4ff821303874f8e02e9d06278dca3bef60
mcteamster <28884260+mcteamster@users.noreply.github.com>
2026-03-17 10:27:07 +1100

    feat: test multiple browsers

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -17,5 +17,9 @@ export default defineConfig({
 
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    { name: 'mobile-chrome', use: { ...devices['Pixel 5'] } },
+    { name: 'mobile-safari', use: { ...devices['iPhone 12'] } },
   ],
 });
