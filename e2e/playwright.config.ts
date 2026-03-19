24814061c14f6f62cb25a03893efa52b37e5cadb
mcteamster <28884260+mcteamster@users.noreply.github.com>
2026-03-19 13:36:25 +1100

    chore: update playwright.config.ts

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -16,10 +16,10 @@ export default defineConfig({
   },
 
   projects: [
-    // { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
     // { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
     // { name: 'webkit', use: { ...devices['Desktop Safari'] } },
     { name: 'mobile-chrome', use: { ...devices['Pixel 5'] } },
-    // { name: 'mobile-safari', use: { ...devices['iPhone 12'] } },
+    { name: 'mobile-safari', use: { ...devices['iPhone 12'] } },
   ],
 });
